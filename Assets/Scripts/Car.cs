1c0d451c403da4697781eb8155a410d51e015a8b
Ivan Golubev <ivan.golubev.spb@gmail.com>
2018-12-02 19:48:05 +0000

    Initial commit
    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    [SerializeField] private WheelCollider frontLeftWheel, frontRightWheel, rearLeftWheel, rearRightWheel;
+    [SerializeField] private Transform frontLeftWheelT, frontRightWheelT, rearLeftWheelT, rearRightWheelT;
+
+    [SerializeField] private float MaxSteeringAngle = 30.0f;
+    [SerializeField] private float MotorForce = 50.0f;
+
+    void FixedUpdate()
+    {
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+        frontLeftWheel.steerAngle = frontRightWheel.steerAngle = horizontalInput * MaxSteeringAngle;
+        frontLeftWheel.motorTorque = frontRightWheel.motorTorque = verticalInput * MotorForce;
+        rearRightWheel.motorTorque = rearLeftWheel.motorTorque = verticalInput * MotorForce;
+
+        UpdateWheelPos(frontLeftWheel, frontLeftWheelT);
+        UpdateWheelPos(frontRightWheel, frontRightWheelT);
+        UpdateWheelPos(rearLeftWheel, rearLeftWheelT);
+        UpdateWheelPos(rearRightWheel, rearRightWheelT);
+    }
+
+    private void UpdateWheelPos(WheelCollider c, Transform t)
+    {
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+
+        c.GetWorldPose(out pos, out rot);
+
+        t.position = pos;
+        t.rotation = rot;
+    }
+
+}

580295408b313c321a4f4afafac16e5f06a7f11a
Ivan Golubev <ivan.golubev.spb@gmail.com>
2018-12-11 22:53:43 +0000

    Camera rotation smoothing
    Camera rotation smoothing

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField] private float smoothspeed = 0.125f;
+    [SerializeField] private float rotationSmoothSpeed = 0.1f;
+    [SerializeField] private Vector3 offset;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position, smoothspeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, rotationSmoothSpeed);        
+    }
+
+}

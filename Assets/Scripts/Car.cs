4b9d6ae9bffa7d5e21131d77e4aced4b3aefad97
Ivan Golubev 🐈 <ivan.golubev@outplay.com>
2018-12-12 19:33:38 +0000

    Adding bg audio and sample touch controls
    Adding bg audio and sample touch controls

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -13,9 +13,7 @@ public class Car : MonoBehaviour {
     [SerializeField] private float MaxSteeringAngle = 30.0f;
     [SerializeField] private float MotorForce = 50.0f;
     [SerializeField] private float DownwardForce = 10.0f;
-
-    [SerializeField] private Vector3 Velocity;
-
+   
     private float verticalInput, horizontalInput;
 
     void Start()
@@ -26,17 +24,65 @@ public class Car : MonoBehaviour {
 
     void Update()
     {
+#if UNITY_EDITOR || UNITY_EDITOR
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+        ProceeMobileInput();
+#endif
     }
+
+#if UNITY_ANDROID || UNITY_IOS
+    // taken from https://unity3d.com/learn/tutorials/projects/2d-roguelike-tutorial/adding-mobile-controls
+    private Vector2 touchOrigin = -Vector2.one; //Used to store location of screen touch origin for mobile controls.
+    private void ProceeMobileInput()
+    {
+        //Check if Input has registered more than zero touches
+        if (Input.touchCount > 0)
+        {
+            //Store the first touch detected.
+            Touch myTouch = Input.touches[0];
+
+            //Check if the phase of that touch equals Began
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                //If so, set touchOrigin to the position of that touch
+                touchOrigin = myTouch.position;
+            }
+
+            //If the touch phase is not Began, and instead is equal to Ended and the x of touchOrigin is greater or equal to zero:
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                //Set touchEnd to equal the position of this touch
+                Vector2 touchEnd = myTouch.position;
+
+                //Calculate the difference between the beginning and end of the touch on the x axis.
+                float x = touchEnd.x - touchOrigin.x;
+
+                //Calculate the difference between the beginning and end of the touch on the y axis.
+                float y = touchEnd.y - touchOrigin.y;
+
+                //Set touchOrigin.x to -1 so that our else if statement will evaluate false and not repeat immediately.
+                touchOrigin.x = -1;
+
+                //Check if the difference along the x axis is greater than the difference along the y axis.
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                    //If x is greater than zero, set horizontal to 1, otherwise set it to -1
+                    horizontalInput = x > 0 ? 1 : -1;
+                else
+                    //If y is greater than zero, set horizontal to 1, otherwise set it to -1
+                    verticalInput = y > 0 ? 1 : -1;
+            }
+        }        
+    }
+#endif
 
     void FixedUpdate()
     {        
         Steer();
         Accelerate();
         RotateWheels();
-        Velocity = CarRigidBody.velocity;
-        //ControlDamping();
         ApplyDownwardForce();
     }
 
@@ -72,17 +118,6 @@ public class Car : MonoBehaviour {
 
         //if (c.isGrounded) { t.position = pos; }
         t.rotation = rot;
-    }
-
-    private void ControlDamping()
-    {
-        float minDamping = 10.0f;
-        float maxDamping = 90.0f;
-        float p = (1 - verticalInput);
-        if (frontLeftWheel != null) { frontLeftWheel.wheelDampingRate = minDamping + p * maxDamping; }
-        if (frontRightWheel != null) { frontRightWheel.motorTorque = minDamping + p * maxDamping; }
-        if (rearRightWheel != null) { rearRightWheel.motorTorque = minDamping + p * maxDamping; }
-        if (rearLeftWheel != null) { rearLeftWheel.motorTorque = minDamping + p * maxDamping; }
     }
 
     private void ApplyDownwardForce() {

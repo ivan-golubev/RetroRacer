30ba47fe97c7b968ec2397c6cda1fcc1c745d34f
Ivan Golubev 🐈 <ivan.golubev@outplay.com>
2018-12-13 12:08:41 +0000

    Adding laser projectile
    Adding laser projectile

---
+++ Assets/Scripts/LaserProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserProjectile : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rigidBody;
+    [SerializeField] private float ThrustMultiplier = 10.0f;
+	
+	void FixedUpdate () {
+        // TODO: the model should be facing the correct direction
+	    rigidBody.AddForce(Vector3.back * ThrustMultiplier);
+    }
+}
---
+++ Assets/Scripts/ShootingComponent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingComponent : MonoBehaviour
+{
+    [SerializeField] private GameObject LaserProjectilePrefab;
+    [SerializeField] private Transform SpawnOrigin;
+
+    void Update()
+    {
+        // TODO: implement the mobile controls
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(LaserProjectilePrefab, SpawnOrigin.position, Quaternion.Euler(90, 0, 0));
+        }
+    }
+
+
+}

b8b73e18acf5050060cd70268b6c53726ef2e41d
Ivan Golubev 🐈 <ivan.golubev@outplay.com>
2018-12-13 14:53:15 +0000

    Lasers: destroying the target
    Lasers: destroying the target

--- Assets/Scripts/LaserProjectile.cs
+++ Assets/Scripts/LaserProjectile.cs
@@ -6,9 +6,11 @@ public class LaserProjectile : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidBody;
     [SerializeField] private float ThrustMultiplier = 10.0f;
+    [SerializeField] private float LifetimeSec = 3.0f;
 
     private Vector3 direction;
     private bool directionSet;
+    private float timeElapsed;
 
     public void SetDirection(Vector3 direction)
     {
@@ -17,17 +19,34 @@ public class LaserProjectile : MonoBehaviour
         directionSet = true;
     }
 
+    void Update()
+    {
+        if (directionSet)
+        {
+            timeElapsed += Time.deltaTime;
+            if (timeElapsed >= LifetimeSec)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
     void FixedUpdate ()
     {
         if (directionSet)
 	    {
             rigidBody.AddForce(direction * ThrustMultiplier);
-        }
+	    }
 	}
 
     void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
+        Obstacle o = collision.gameObject.GetComponent<Obstacle>();
+        if (o != null)
+        {
+            o.TakeLaserDamage();
+        }
     }
 
 }
